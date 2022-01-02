9f87901d515138dd246250ea993271d1977bacb9
Ruth <ruth@Deniss-Air.home>
2022-01-02 19:13:08 +0100

    nivell1

---
+++ typescript.ts
@@ -0,0 +1,53 @@
+function getJoke():void{
+  fetch('https://icanhazdadjoke.com/', {
+    headers:{
+      Accept: "application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(
+    data => {
+      document.getElementById("joke").innerHTML = `"${data.joke}"`
+      joke= data.joke
+
+    }
+  )}
+
+const reportJokes=[];
+let joke:string 
+let score:number = 0
+const date = new Date()
+
+document.querySelector(".bad").addEventListener("click",function(){
+ score = 1
+});
+
+document.querySelector(".ok").addEventListener("click",function(){
+  score = 2
+ });
+
+ document.querySelector(".great").addEventListener("click",function(){
+  score = 3
+ });
+
+
+document.getElementById("next").addEventListener("click", function () {
+  getJoke()
+  if(joke){
+    reportJokes.push(
+      {joke: joke,
+      date: date.toISOString(),
+      score: score
+      }
+    )
+    console.log(reportJokes)
+  }
+  
+})
+
+
+
+
+
+
+
